a7a6aee23bf675e6168de724f06cc0e536b26281
碎碎酱 <yinxin630@gmail.com>
2016-05-05 17:18:11 +0800

    feat(wave): add wave

--- src/effects/effects.js
+++ src/effects/effects.js
@@ -1,9 +1,11 @@
 import fade from './fade.js';
 import scale from './scale.js';
+import wave from './wave.js';
 
 const effects = {
     fade,
-    scale
+    scale,
+    wave
 };
 
 export default effects;
---
+++ src/effects/wave.js
@@ -0,0 +1,33 @@
+import transition from '../utils/transition.js';
+
+const Wave = {
+    componentWillEnter (duration) {
+        transition({
+            0: {
+                transform: 'scale(1)'
+            },
+            33: {
+                transform: 'scale(1.05)'
+            },
+            66: {
+                transform: 'scale(0.97)'
+            },
+            100: {
+                transform: 'scale(1)'
+            }
+        }, duration, this);
+    },
+    
+    componentWillLeave (duration) {
+        transition({
+            0: {
+                transform: 'scale(1)'
+            },
+            100: {
+                transform: 'scale(0.5)'
+            }
+        }, duration, this);
+    }
+};
+
+export default Wave;
