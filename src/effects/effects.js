863f0518ff64560526908287a98cbb069e7131ea
碎碎酱 <yinxin630@gmail.com>
2016-05-05 17:48:58 +0800

    feat(fly): add fly

--- src/effects/effects.js
+++ src/effects/effects.js
@@ -3,13 +3,15 @@ import scale from './scale.js';
 import wave from './wave.js';
 import rotate from './rotate.js';
 import drop from './drop.js';
+import fly from './fly.js';
 
 const effects = {
     fade,
     scale,
     wave,
     rotate,
-    drop
+    drop,
+    fly
 };
 
 export default effects;
---
+++ src/effects/fly.js
@@ -0,0 +1,31 @@
+import transition from '../utils/transition.js';
+
+const Fly = {
+    componentWillEnter (duration) {
+        transition({
+            0: {
+                transform: 'translate(-200px, 0px)',
+                opacity: 0.01
+            },
+            100: {
+                transform: 'translate(0px, 0px)',
+                opacity: 1
+            }
+        }, duration, this);
+    },
+    
+    componentWillLeave (duration) {
+        transition({
+            0: {
+                transform: 'translate(0px, 0px)',
+                opacity: 1
+            },
+            100: {
+                transform: 'translate(200px, 0px)',
+                opacity: 0.01
+            }
+        }, duration, this);
+    }
+};
+
+export default Fly;
