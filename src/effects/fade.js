62628cc810a390bb9ee87899c837adcd559f5121
碎碎酱 <yinxin630@gmail.com>
2016-05-05 13:30:31 +0800

    refactor(fade): delete annotations

--- src/effects/fade.js
+++ src/effects/fade.js
@@ -1,30 +1,8 @@
 import transition from '../utils/transition.js';
 
-// const fade = {
-//     componentWillEnter (duration) {
-//         transition.begin(this, duration * 0.5, {
-//             color: 'red',
-//             backgroundColor: 'red'
-//         });
-        
-//         transition.end(this, duration * 0.5, {
-//             color: 'blue',
-//             transform: 'scale(2)',
-//         });
-        
-//         setTimeout(() => {
-//             transition.end(this, duration * 0.5, {
-//             color: 'pink',
-//             transform: 'scale(2) scale(0.5)',
-//             backgroundColor: 'blue'
-//         });
-//         }, duration * 0.5);
-        
-//     }
-// };
-
 const fade = {
     componentWillEnter (duration) {
+        console.log(this);
         transition({
             0: {
                 backgroundColor: 'red'
